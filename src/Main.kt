bff20ba92f6a8cce7b3f954e8d33ce88817fb989
Raí Rodrigues <rairodrigues040@gmail.com>
2024-12-10 17:34:21 -0300

    Commit do dia

--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,13 @@ import kotlin.math.roundToInt
 
 fun main() {
     val teste = Flutuante()
-    teste.calculo(576.73f)
+    teste.calculo(576.24)
 }
 
 class Flutuante {
-    fun calculo(valor: Float) {
+    fun calculo(valor: Double) {
         val notas: List<Int> = listOf(100, 50, 20, 10, 5, 2)
-        val moedas: List<Float> = listOf(1.00f, 0.50f, 0.25f, 0.10f, 0.05f, 0.01f)
+        val moedas: List<Double> = listOf(1.00, 0.50, 0.25, 0.10, 0.05, 0.01)
 
         var valorRestante = valor
 
@@ -19,15 +19,12 @@ class Flutuante {
             println("$quantidadeNotas nota(s) de R$ $nota.00")
         }
 
-        // Convertendo para centavos (usando arredondamento para evitar problemas de precisão)
-        var valorMoedas = valorRestante
-
         println("MOEDAS:")
         for (moeda in moedas) {
-            val valorMoedaEmCentavos = (moeda * 100).toInt()
-            val quantidadeMoedas = valorRestanteCentavos / valorMoedaEmCentavos
-            valorRestanteCentavos %= valorMoedaEmCentavos
-            println("$quantidadeMoedas moeda(s) de R$ ${"%.2f".format(moeda)}")
+            val quantidadeMoedas = (valorRestante / moeda).toInt()
+            valorRestante %= moeda
+
+            println("$quantidadeMoedas moeda(s) de R$ %.2f".format(moeda))
         }
     }
 }
